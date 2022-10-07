cd1aa71416955f14062cc994629984a3082e95e2
cayto <aidarous1143@gmail.com>
2022-10-07 19:54:15 +0300

    🔨build database

--- database/migrations/20221004075135_onlineschool.js
+++ database/migrations/20221004075135_onlineschool.js
@@ -26,26 +26,50 @@ exports.up = function (knex) {
           .onDelete("RESTRICT");
     })
     .createTable("courses", (tbl) => {
-      tbl.increments("user_id"),
-        tbl.string("username", 128).notNullable(),
-        tbl.string("email", 128).notNullable(),
-        tbl.integer("password", 128).notNullable(),
-        tbl.binary("profileImage"),
-        tbl.string("location", 128),
+      tbl.increments("course_id"),
+        tbl.string("course_title", 128).notNullable(),
+        tbl.string("description").notNullable(),
         tbl.string("create_at").notNullable(),
-        tbl.string("update_at");
+        tbl.string("update_at"),
+        tbl
+          .integer("user_id")
+          .notNullable()
+          .references("user_id")
+          .inTable("users")
+          .onUpdate("RESTRICT")
+          .onDelete("RESTRICT");
+    })
+    .createTable("chapters", (tbl) => {
+        tbl.increments("chapter_id"),
+        tbl.string("chapter_title", 128).notNullable(),
+        tbl
+          .integer("course_id")
+          .notNullable()
+          .references("course_id")
+          .inTable("courses")
+          .onUpdate("RESTRICT")
+          .onDelete("RESTRICT");
+    })
+    .createTable("videos", (tbl) => {
+      tbl.increments("video_id"),
+        tbl.binary("video").notNullable(),
+        tbl.string("video_title").notNullable(),
+        tbl
+          .integer("course_id")
+          .notNullable()
+          .references("course_id")
+          .inTable("courses")
+          .onUpdate("RESTRICT")
+          .onDelete("RESTRICT");
+      tbl
+        .integer("chapter_id")
+        .notNullable()
+        .references("chapter_id")
+        .inTable("chapters")
+        .onUpdate("RESTRICT")
+        .onDelete("RESTRICT");
     })
     .createTable("reviews", (tbl) => {
-      tbl.increments("user_id"),
-        tbl.string("username", 128).notNullable(),
-        tbl.string("email", 128).notNullable(),
-        tbl.integer("password", 128).notNullable(),
-        tbl.binary("profileImage"),
-        tbl.string("location", 128),
-        tbl.string("create_at").notNullable(),
-        tbl.string("update_at");
-    })
-    .createTable("video", (tbl) => {
       tbl.increments("user_id"),
         tbl.string("username", 128).notNullable(),
         tbl.string("email", 128).notNullable(),
@@ -63,7 +87,9 @@ exports.up = function (knex) {
  */
 exports.down = function (knex) {
   return knex.schema
-    .dropTableIfExists("courses")
     .dropTableIfExists("users")
+    .dropTableIfExists("courses")
+    .dropTableIfExists("chapters")
+    .dropTableIfExists("videos")
     .dropTableIfExists("roles");
 };
